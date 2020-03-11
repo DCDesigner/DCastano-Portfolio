6bf4066bdf251ed49bfdd76e0e21c3c104cbb482
Deanna Castano <paisitadc8@gmail.com>
2020-03-11 16:19:03 +0000

    adding website

---
+++ assets/js/backtotop_button.js
@@ -0,0 +1,18 @@
+
+/*-- Back to top btn --*/
+
+let btn = $('#back-to-top');
+
+$(window).scroll(() => {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', (e) => {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
---
+++ assets/js/progressbar.js
@@ -0,0 +1,16 @@
+/* https://codepen.io/angel_crawford/pen/pRrvbG */
+
+var delay = 900;
+$(".progress-bar").each(function(i){
+    $(this).delay( delay*i ).animate( { width: $(this).attr('aria-valuenow') + '%' }, delay );
+
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: delay,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now)+'%');
+        }
+    });
+});
